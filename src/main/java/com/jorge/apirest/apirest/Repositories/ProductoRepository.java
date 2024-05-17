6c7ef250853b4a78df720a9b7a8c3e1868f58c70
JorgeRJ85 <jorgeriverosjofre@gmail.com>
2024-05-17 14:52:51 -0300

    Version Railway

---
+++ src/main/java/com/jorge/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.jorge.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jorge.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository <Producto, Long> {
+
+}
